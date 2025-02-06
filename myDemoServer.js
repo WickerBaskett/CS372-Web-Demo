78d35d1591a60c10fff36662a52de87c2e2e0202
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-02-06 14:51:35 -0900

    Got retrieve from mongodb working, client is closed after one connection still

--- myDemoServer.js
+++ myDemoServer.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 6543;
 
 app.use(express.static(path.join(__dirname, "")));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "", 'myDemoWebpage.html'));
@@ -16,10 +16,16 @@ app.get('/', (req, res) => {
 const util = require('util');
 
 app.post("/insert", (req, res) => {
-    //console.log("Request Recieved: " + req.body);
-    console.log(`post/${util.inspect(req.body,false,null)}`);
     mongo_insert(req.body.db_input).catch(console.dir);
-})
+});
+
+app.get("/retrieve", (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    console.log("Retrieving data from MongoDB");
+    mongo_retrieve().then((result) => {
+        res.send(result);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port 6543`);
@@ -29,17 +35,30 @@ const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 async function mongo_insert(new_entry) {
-    console.log("mongo_insert() called")
     try {
         const mydatabase = client.db("myDemoDb");
         const mycollection = mydatabase.collection("myDemoCollection");
-        console.log(new_entry);
         // create a document to insert
         const doc = {
-            test: 'new_entry',
+            EnteredName: new_entry,
         }
         const result = await mycollection.insertOne(doc);
         console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        await client.close();
+    }
+}
+
+async function mongo_retrieve() {
+    try {
+        // define a database and collection on which to run the method
+        const database = client.db("myDemoDb");
+        const coll = database.collection("myDemoCollection");
+        console.log("Sending request to db");
+        const distinctValues = await coll.find();
+        console.log("Returning from mongo_retrieve()");
+        const data = await distinctValues.toArray();
+        return data;
     } finally {
         await client.close();
     }
