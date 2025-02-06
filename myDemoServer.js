18f2b3b18c1d3f23d0f1d3a7e0b418f9e4696857
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-02-05 19:18:00 -0900

    Can insert into db, getting user text is broken again

--- myDemoServer.js
+++ myDemoServer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const MongoClient =  require('mongodb').MongoClient;
+const { MongoClient } =  require('mongodb');
 
 
 const app = express();
@@ -14,38 +14,30 @@ app.get('/', (req, res) => {
 });
 
 app.get("/insert", (req, res) => {
-    console.log("Request Recieved");
-    mongo_insert(req.query.db_input);
+    console.log("Request Recieved: " + req.query.db_input);
+    mongo_insert(req.query.db_input).catch(console.dir);
 })
 
 app.listen(port, () => {
     console.log(`Server running on port 6543`);
 });
 
-const uri = 'mongodb://localhost:27017';
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
 
 async function mongo_insert(new_entry) {
     console.log("mongo_insert() called")
-    // We assign a callback here to connect to the db and insert the entry
-    //    before closing the db connection
-    MongoClient.connect(uri, function(err, db) {
-        console.log("db connection established");
-
-        if (err) throw err;
-
-        const my_db = db.db('myDemoDb');
-        const collection = my_db.collection('myDemoCollection');
-
-        // This is an async function call so we asign a callback to check
-        //   if we succesfully inserted into the db
-        collection.insertOne({
-            name: new_entry
-          }, function(err, res) {
-            
-            if (err) throw err;
-
-            console.log(new_entry + " inserted into db");
-            db.close();
-          });
-    })
+    try {
+        const mydatabase = client.db("myDemoDb");
+        const mycollection = mydatabase.collection("myDemoCollection");
+        console.log(new_entry);
+        // create a document to insert
+        const doc = {
+            test: 'new_entry',
+        }
+        const result = await mycollection.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        await client.close();
+    }
 }
