6fa778a50c09f0a1b2f19b06d5755971a27e97d9
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-02-05 18:54:17 -0900

    Work done up to Hardware lab, getting text from the input box on client side successfully

--- myDemoServer.js
+++ myDemoServer.js
@@ -1,35 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const MongoClient =  require('mongodb').MongoClient;
 
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const app = express();
+const port = 6543;
 
-// Create Node.js server
-http.createServer(function (req, res) {
+app.use(express.static(path.join(__dirname, "")));
 
-    fs.readFile('myDemoWebpage.html', function(err, data) {
-      currentUrl = url.parse(req.url, true);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data); // Print info to the client-side
-      return res.end();
-    });
-  
-  }).listen(6543);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "", 'myDemoWebpage.html'));
+});
 
-const { MongoClient } = require('mongodb');
+app.get("/insert", (req, res) => {
+    console.log("Request Recieved");
+    mongo_insert(req.query.db_input);
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port 6543`);
+});
+
 const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
 
-try {
-    await client.connect();
-    const db = client.db('myDemoDb');
-    const collection = db.collection('myDemoCollection');
+async function mongo_insert(new_entry) {
+    console.log("mongo_insert() called")
+    // We assign a callback here to connect to the db and insert the entry
+    //    before closing the db connection
+    MongoClient.connect(uri, function(err, db) {
+        console.log("db connection established");
 
-    // Find the first document in the collection
-    const first = await collection.findOne();
-    console.log(first);
+        if (err) throw err;
 
-} finally {
-    await client.close();
+        const my_db = db.db('myDemoDb');
+        const collection = my_db.collection('myDemoCollection');
+
+        // This is an async function call so we asign a callback to check
+        //   if we succesfully inserted into the db
+        collection.insertOne({
+            name: new_entry
+          }, function(err, res) {
+            
+            if (err) throw err;
+
+            console.log(new_entry + " inserted into db");
+            db.close();
+          });
+    })
 }
-//run().catch(console.error);
